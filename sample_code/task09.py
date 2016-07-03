5d9263c3cbd092ecee9a3a7a02e1f7d962a766df
国里愛彦 <ykunisato@gmail.com>
2016-07-03 23:49:10 +0900

    説明の追加，サンプルコード9と10の修正

--- sample_code/task09.py
+++ sample_code/task09.py
@@ -2,24 +2,44 @@
 from psychopy import visual, core, event, gui, data, misc
 import numpy, os, random, time, csv
 
+#刺激セットの繰り返し数
+M = 2
+
 try:
     #　画面の準備（灰色の画面、マウスはallowGUI=Falseで表示されないようにしている）
     myWin = visual.Window (fullscr=True, monitor= 'Default', allowGUI=False, units='norm', color= (0,0,0))
-    # 文字（漢字)のリスト
-    kanjiList = [u'赤',u'黄',u'青',u'赤',u'黄',u'青',u'赤',u'黄',u'青']
-    # 色のリスト(visual.TextStimのcolorにいれる)
-    colorList = [(1,-1,-1),(1,-1,-1),(1,-1,-1),(1,1,-1),(1,1,-1),(1,1,-1),(-1,-1,1),(-1,-1,1),(-1,-1,1)]
-    # ランダムに提示するためのリスト
-    randomList = range(9)
-    # 内側のforループを2回繰り返すためのfor文
-    for m in range(2):
-        # randomListをシャッフルする。
-        numpy.random.shuffle(randomList)
-        # 内側のfor文（range(9)で0~8のリストを作成し、前から順番でiにいれる）
-        for i in range(9):
-            #　kanjiListのrandomList[i]番目（kanjiList[randomList[i]]）を、
-            # colorListのrandomList[i]番目の色(colorList[randomList[i]])で提示する。
-            myText = visual.TextStim(myWin,text = kanjiList[randomList[i]],pos=(0,0),color = colorList[randomList[i]],height=0.2)
+
+    # 色の辞書の作成
+    colorDic = {
+            u'赤': {'rgb': ( 1, -1,-1), 'type': '0'},
+            u'黄': {'rgb': ( 1,  1,-1), 'type': '1'},
+            u'青': {'rgb': (-1, -1, 1), 'type': '2'}
+            }
+    #　文字と文字の色の辞書の作成
+    charConditionList = [
+            {'kanjiChar': u'赤', 'color': u'赤'},
+            {'kanjiChar': u'黄', 'color': u'赤'},
+            {'kanjiChar': u'青', 'color': u'赤'},
+            {'kanjiChar': u'赤', 'color': u'黄'},
+            {'kanjiChar': u'黄', 'color': u'黄'},
+            {'kanjiChar': u'青', 'color': u'黄'},
+            {'kanjiChar': u'赤', 'color': u'青'},
+            {'kanjiChar': u'黄', 'color': u'青'},
+            {'kanjiChar': u'青', 'color': u'青'}
+            ]
+    # 辞書の長さ（刺激セット数）
+    N = len(charConditionList)
+
+    # 内側のforループをM回繰り返すためのfor文
+    for m in range(M):
+        r = range(N)
+        numpy.random.shuffle(r)
+        for i, currentState in enumerate(r):
+            charCondition = charConditionList[currentState]
+            colorData = colorDic[charCondition['color']]
+            kanjiCharData = colorDic[charCondition['kanjiChar']]
+            #　kanjiListのi番目（kanjiList[i]）を、colorListのi番目の色(colorList[i])で提示する。
+            myText = visual.TextStim(myWin,text = charCondition['kanjiChar'],pos=(0,0),color = colorData['rgb'],height=0.2)
             myText.draw()
             myWin.flip()
             core.wait(1)
